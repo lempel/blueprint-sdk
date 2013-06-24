8ebb68d59ee9431c7b2c335395687afe7e72d71a
Simon Lee <lempel@gmail.com>
2013-06-25 00:26:05 +0900

    jdbc package added

---
+++ blueprint-sdk/src/blueprint/sdk/util/jdbc/MdbConnector.java
@@ -0,0 +1,77 @@
+/*
+ License:
+
+ blueprint-sdk is licensed under the terms of Eclipse Public License(EPL) v1.0
+ (http://www.eclipse.org/legal/epl-v10.html)
+
+
+ Distribution:
+
+ Repository - https://github.com/lempel/blueprint-sdk.git
+ Blog - http://lempel.egloos.com
+ */
+
+package blueprint.sdk.util.jdbc;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+import blueprint.sdk.util.Validator;
+
+/**
+ * Creates java.sql.Connection instance for MDB file(MS Access)
+ * 
+ * @author Simon Lee
+ * @since 2007. 08. 07
+ */
+public class MdbConnector {
+	private static final String JDBC_DRIVER_CLASS = "sun.jdbc.odbc.JdbcOdbcDriver";
+	private static final String EMPTY_STRING = "";
+	private static final String URL_PREFIX = "jdbc:odbc:Driver={Microsoft Access Driver (*.mdb)};DBQ=";
+
+	private transient Connection connection = null;
+
+	public boolean connect(final String mdbPath) throws ClassNotFoundException {
+		// Initialize the JdbcOdbc Bridge Driver
+		Class.forName(JDBC_DRIVER_CLASS);
+
+		boolean result = false;
+		try {
+			// DSN & Property set
+			connection = getAccessDBConnection(mdbPath);
+			result = true;
+		} catch (SQLException se) {
+			close();
+		}
+
+		return result;
+	}
+
+	private Connection getAccessDBConnection(final String strDBPath) throws SQLException {
+		String path = strDBPath.replace('\\', '/').trim();
+		String databaseURL = URL_PREFIX + path;
+
+		return DriverManager.getConnection(databaseURL, EMPTY_STRING, EMPTY_STRING);
+	}
+
+	public Connection getConnection() {
+		return connection;
+	}
+
+	public void close() {
+		try {
+			if (Validator.isNotNull(connection)) {
+				connection.close();
+			}
+		} catch (SQLException ignored) {
+		}
+	}
+
+	@Override
+	protected void finalize() throws Throwable {
+		close();
+
+		super.finalize();
+	}
+}
---
+++ blueprint-sdk/src/blueprint/sdk/util/jdbc/ResultSetHelper.java
@@ -0,0 +1,271 @@
+/*
+ License:
+
+ blueprint-sdk is licensed under the terms of Eclipse Public License(EPL) v1.0
+ (http://www.eclipse.org/legal/epl-v10.html)
+
+
+ Distribution:
+
+ Repository - https://github.com/lempel/blueprint-sdk.git
+ Blog - http://lempel.egloos.com
+ */
+
+package blueprint.sdk.util.jdbc;
+
+import java.io.InputStream;
+import java.io.Reader;
+import java.math.BigDecimal;
+import java.sql.Blob;
+import java.sql.Clob;
+import java.sql.Date;
+import java.sql.ResultSet;
+import java.sql.ResultSetMetaData;
+import java.sql.SQLException;
+import java.sql.SQLWarning;
+import java.sql.Statement;
+import java.sql.Time;
+import java.sql.Timestamp;
+
+/**
+ * Prevent NullPointerException from ResultSet.<br>
+ * (getString, getBytes)<br>
+ * 
+ * @author Simon Lee
+ * @since 2009. 2. 26.
+ */
+public class ResultSetHelper {
+	private transient ResultSet rset;
+
+	public ResultSetHelper(final ResultSet rset) {
+		this.rset = rset;
+	}
+
+	public boolean next() throws SQLException {
+		return rset.next();
+	}
+
+	public void close() throws SQLException {
+		rset.close();
+	}
+
+	public void wasNull() throws SQLException {
+		rset.wasNull();
+	}
+
+	public String getString(final int index) throws SQLException {
+		String result = rset.getString(index);
+		if (result == null) {
+			result = "";
+		}
+		return result.trim();
+	}
+
+	public boolean getBoolean(final int index) throws SQLException {
+		return rset.getBoolean(index);
+	}
+
+	public byte getByte(final int index) throws SQLException {
+		return rset.getByte(index);
+	}
+
+	public int getInt(final int index) throws SQLException {
+		return rset.getInt(index);
+	}
+
+	public long getLong(final int index) throws SQLException {
+		return rset.getLong(index);
+	}
+
+	public float getFloat(final int index) throws SQLException {
+		return rset.getFloat(index);
+	}
+
+	public double getDouble(final int index) throws SQLException {
+		return rset.getDouble(index);
+	}
+
+	public BigDecimal getBigDecimal(final int index) throws SQLException {
+		return rset.getBigDecimal(index);
+	}
+
+	public byte[] getBytes(final int index) throws SQLException {
+		byte[] result = rset.getBytes(index);
+		if (result == null) {
+			result = new byte[0];
+		}
+		return result;
+	}
+
+	public Date getDate(final int index) throws SQLException {
+		return rset.getDate(index);
+	}
+
+	public Time getTime(final int index) throws SQLException {
+		return rset.getTime(index);
+	}
+
+	public Timestamp getTimestamp(final int index) throws SQLException {
+		return rset.getTimestamp(index);
+	}
+
+	public InputStream getAsciiStream(final int index) throws SQLException {
+		return rset.getAsciiStream(index);
+	}
+
+	public InputStream getBinaryStream(final int index) throws SQLException {
+		return rset.getBinaryStream(index);
+	}
+
+	public String getString(final String index) throws SQLException {
+		String result = rset.getString(index);
+		if (result == null) {
+			result = "";
+		}
+		return result.trim();
+	}
+
+	public boolean getBoolean(final String index) throws SQLException {
+		return rset.getBoolean(index);
+	}
+
+	public byte getByte(final String index) throws SQLException {
+		return rset.getByte(index);
+	}
+
+	public int getInt(final String index) throws SQLException {
+		return rset.getInt(index);
+	}
+
+	public long getLong(final String index) throws SQLException {
+		return rset.getLong(index);
+	}
+
+	public float getFloat(final String index) throws SQLException {
+		return rset.getFloat(index);
+	}
+
+	public double getDouble(final String index) throws SQLException {
+		return rset.getDouble(index);
+	}
+
+	public BigDecimal getBigDecimal(final String index) throws SQLException {
+		return rset.getBigDecimal(index);
+	}
+
+	public byte[] getBytes(final String index) throws SQLException {
+		byte[] result = rset.getBytes(index);
+		if (result == null) {
+			result = new byte[0];
+		}
+		return result;
+	}
+
+	public Date getDate(final String index) throws SQLException {
+		return rset.getDate(index);
+	}
+
+	public Time getTime(final String index) throws SQLException {
+		return rset.getTime(index);
+	}
+
+	public Timestamp getTimestamp(final String index) throws SQLException {
+		return rset.getTimestamp(index);
+	}
+
+	public InputStream getAsciiStream(final String index) throws SQLException {
+		return rset.getAsciiStream(index);
+	}
+
+	public InputStream getBinaryStream(final String index) throws SQLException {
+		return rset.getBinaryStream(index);
+	}
+
+	public SQLWarning getWarnings() throws SQLException {
+		return rset.getWarnings();
+	}
+
+	public void clearWarnings() throws SQLException {
+		rset.clearWarnings();
+	}
+
+	public String getCursorName() throws SQLException {
+		return rset.getCursorName();
+	}
+
+	public ResultSetMetaData getMetaData() throws SQLException {
+		return rset.getMetaData();
+	}
+
+	public Object getObject(final int index) throws SQLException {
+		return rset.getObject(index);
+	}
+
+	public Object getObject(final String index) throws SQLException {
+		return rset.getObject(index);
+	}
+
+	public int findColumn(final String index) throws SQLException {
+		return rset.findColumn(index);
+	}
+
+	public Reader getCharacterStream(final int index) throws SQLException {
+		return rset.getCharacterStream(index);
+	}
+
+	public Reader getCharacterStream(final String index) throws SQLException {
+		return rset.getCharacterStream(index);
+	}
+
+	public boolean first() throws SQLException {
+		return rset.first();
+	}
+
+	public boolean last() throws SQLException {
+		return rset.last();
+	}
+
+	public int getRow() throws SQLException {
+		return rset.getRow();
+	}
+
+	public boolean previous() throws SQLException {
+		return rset.previous();
+	}
+
+	public void setFetchDirection(final int dir) throws SQLException {
+		rset.setFetchDirection(dir);
+	}
+
+	public int getFetchDirection() throws SQLException {
+		return rset.getFetchDirection();
+	}
+
+	public void setFetchSize(final int dir) throws SQLException {
+		rset.setFetchSize(dir);
+	}
+
+	public int getFetchSize() throws SQLException {
+		return rset.getFetchSize();
+	}
+
+	public Statement getStatement() throws SQLException {
+		return rset.getStatement();
+	}
+
+	public Blob getBlob(final int index) throws SQLException {
+		return rset.getBlob(index);
+	}
+
+	public Clob getClob(final int index) throws SQLException {
+		return rset.getClob(index);
+	}
+
+	public Blob getBlob(final String index) throws SQLException {
+		return rset.getBlob(index);
+	}
+
+	public Clob getClob(final String index) throws SQLException {
+		return rset.getClob(index);
+	}
+}
