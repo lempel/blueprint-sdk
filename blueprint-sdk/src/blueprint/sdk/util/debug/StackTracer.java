01148dd570ae142761c0b675ea39e341df1d9642
Simon Lee <lempel@gmail.com>
2013-09-05 22:31:56 +0900

    StackTracer added

---
+++ blueprint-sdk/src/blueprint/sdk/util/debug/StackTracer.java
@@ -0,0 +1,37 @@
+/*
+ License:
+
+ blueprint-sdk is licensed under the terms of Eclipse Public License(EPL) v1.0
+ (http://www.eclipse.org/legal/epl-v10.html)
+
+
+ Distribution:
+
+ Repository - https://github.com/lempel/blueprint-sdk.git
+ Blog - http://lempel.egloos.com
+ */
+
+package blueprint.sdk.util.debug;
+
+import org.apache.log4j.Logger;
+
+/**
+ * Utility for stack trace
+ * 
+ * @author Simon Lee
+ * @since 2013. 9. 5.
+ */
+public class StackTracer {
+	private static final Logger L = Logger.getLogger(StackTracer.class);
+
+	/**
+	 * Prints current stack trace
+	 */
+	public static void printStackTrace() {
+		try {
+			throw new Exception("printStackTrace");
+		} catch (Exception e) {
+			L.info("printStackTrace", e);
+		}
+	}
+}
