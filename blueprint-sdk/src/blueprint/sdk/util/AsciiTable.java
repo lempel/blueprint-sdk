dc4a61d627a090ff95df8dadb4487fd49d63ed49
Sangmin Lee <lempel@gmail.com>
2015-12-21 16:18:31 +0900

    ASCII table added

---
+++ blueprint-sdk/src/blueprint/sdk/util/AsciiTable.java
@@ -0,0 +1,522 @@
+package blueprint.sdk.util;
+
+/**
+ * ASCII Code Table
+ *
+ * @author Sangmi Lee
+ * @Since 2015-12-21
+ */
+public class AsciiTable {
+    /**
+     * ASCII Code : NULL
+     */
+    public static final byte ASCII_NULL = (byte) 0x00;
+    /**
+     * ASCII Code : Start of heading
+     */
+    public static final byte ASCII_SOH = (byte) 0x01;
+    /**
+     * ASCII Code : Start of text
+     */
+    public static final byte ASCII_STX = (byte) 0x02;
+    /**
+     * ASCII Code : End pf text
+     */
+    public static final byte ASCII_ETX = (byte) 0x03;
+    /**
+     * ASCII Code : End of transmission
+     */
+    public static final byte ASCII_EOT = (byte) 0x04;
+    /**
+     * ASCII Code : Enquiry
+     */
+    public static final byte ASCII_ENQ = (byte) 0x05;
+    /**
+     * ASCII Code : Acknowledge
+     */
+    public static final byte ASCII_ACK = (byte) 0x06;
+    /**
+     * ASCII Code : Bell
+     */
+    public static final byte ASCII_BELL = (byte) 0x07;
+    /**
+     * ASCII Code : Backspace
+     */
+    public static final byte ASCII_BS = (byte) 0x08;
+    /**
+     * ASCII Code : Horizontal tab
+     */
+    public static final byte ASCII_TAB = (byte) 0x09;
+    /**
+     * ASCII Code : NL line feed, new line
+     */
+    public static final byte ASCII_LF = (byte) 0x0a;
+    /**
+     * ASCII Code : Vertical tab
+     */
+    public static final byte ASCII_VT = (byte) 0x0b;
+    /**
+     * ASCII Code : NP form feed, new page
+     */
+    public static final byte ASCII_FF = (byte) 0x0c;
+    /**
+     * ASCII Code : Carriage return
+     */
+    public static final byte ASCII_CR = (byte) 0x0d;
+    /**
+     * ASCII Code : Shift out
+     */
+    public static final byte ASCII_SO = (byte) 0x0e;
+    /**
+     * ASCII Code : Shift in
+     */
+    public static final byte ASCII_SI = (byte) 0x0f;
+    /**
+     * ASCII Code : Data line esacpe
+     */
+    public static final byte ASCII_DLE = (byte) 0x10;
+    /**
+     * ASCII Code : Device control 1
+     */
+    public static final byte ASCII_DC1 = (byte) 0x11;
+    /**
+     * ASCII Code : Device control 2
+     */
+    public static final byte ASCII_DC2 = (byte) 0x12;
+    /**
+     * ASCII Code : Device control 3
+     */
+    public static final byte ASCII_DC3 = (byte) 0x13;
+    /**
+     * ASCII Code : Device control 4
+     */
+    public static final byte ASCII_DC4 = (byte) 0x14;
+    /**
+     * ASCII Code : Negative acknowledge
+     */
+    public static final byte ASCII_NAK = (byte) 0x15;
+    /**
+     * ASCII Code : Synchronous idle
+     */
+    public static final byte ASCII_SYN = (byte) 0x16;
+    /**
+     * ASCII Code : End of transmission block
+     */
+    public static final byte ASCII_ETB = (byte) 0x17;
+    /**
+     * ASCII Code : Cancel
+     */
+    public static final byte ASCII_CAN = (byte) 0x18;
+    /**
+     * ASCII Code : End of medium
+     */
+    public static final byte ASCII_EM = (byte) 0x19;
+    /**
+     * ASCII Code : Substitute
+     */
+    public static final byte ASCII_SUB = (byte) 0x1a;
+    /**
+     * ASCII Code : Escape
+     */
+    public static final byte ASCII_ESC = (byte) 0x1b;
+    /**
+     * ASCII Code : File Separator
+     */
+    public static final byte ASCII_FS = (byte) 0x1c;
+    /**
+     * ASCII Code : Group Separator
+     */
+    public static final byte ASCII_GS = (byte) 0x1d;
+    /**
+     * ASCII Code : Record Separator
+     */
+    public static final byte ASCII_RS = (byte) 0x1e;
+    /**
+     * ASCII Code : Unit separator
+     */
+    public static final byte ASCII_US = (byte) 0x1f;
+    /**
+     * ASCII Code : ' '
+     */
+    public static final byte ASCII_SPACE = (byte) 0x20;
+    /**
+     * ASCII Code : '!'
+     */
+    public static final byte ASCII_EXCLAMATION = (byte) 0x21;
+    /**
+     * ASCII Code : '"'
+     */
+    public static final byte ASCII_DOUBLE_QUOTE = (byte) 0x22;
+    /**
+     * ASCII Code : '#'
+     */
+    public static final byte ASCII_SHARP = (byte) 0x23;
+    /**
+     * ASCII Code : '$'
+     */
+    public static final byte ASCII_DOLLAR = (byte) 0x24;
+    /**
+     * ASCII Code : '%'
+     */
+    public static final byte ASCII_PERCENT = (byte) 0x25;
+    /**
+     * ASCII Code : '&'
+     */
+    public static final byte ASCII_AND = (byte) 0x26;
+    /**
+     * ASCII Code : '''
+     */
+    public static final byte ASCII_QUOTE = (byte) 0x27;
+    /**
+     * ASCII Code : '('
+     */
+    public static final byte ASCII_L_PAREN = (byte) 0x28;
+    /**
+     * ASCII Code : ')'
+     */
+    public static final byte ASCII_R_PAREN = (byte) 0x29;
+    /**
+     * ASCII Code : '*'
+     */
+    public static final byte ASCII_MULTIPLY = (byte) 0x2a;
+    /**
+     * ASCII Code : '+'
+     */
+    public static final byte ASCII_PLUS = (byte) 0x2b;
+    /**
+     * ASCII Code : ','
+     */
+    public static final byte ASCII_COMMA = (byte) 0x2c;
+    /**
+     * ASCII Code : '-'
+     */
+    public static final byte ASCII_DASH = (byte) 0x2d;
+    /**
+     * ASCII Code : '.'
+     */
+    public static final byte ASCII_DOT = (byte) 0x2e;
+    /**
+     * ASCII Code : '/'
+     */
+    public static final byte ASCII_SLASH = (byte) 0x2f;
+    /**
+     * ASCII Code : '0'
+     */
+    public static final byte ASCII_0 = (byte) 0x30;
+    /**
+     * ASCII Code : '1'
+     */
+    public static final byte ASCII_1 = (byte) 0x31;
+    /**
+     * ASCII Code : '2'
+     */
+    public static final byte ASCII_2 = (byte) 0x32;
+    /**
+     * ASCII Code : '3'
+     */
+    public static final byte ASCII_3 = (byte) 0x33;
+    /**
+     * ASCII Code : '4'
+     */
+    public static final byte ASCII_4 = (byte) 0x34;
+    /**
+     * ASCII Code : '5'
+     */
+    public static final byte ASCII_5 = (byte) 0x35;
+    /**
+     * ASCII Code : '6'
+     */
+    public static final byte ASCII_6 = (byte) 0x36;
+    /**
+     * ASCII Code : '7'
+     */
+    public static final byte ASCII_7 = (byte) 0x37;
+    /**
+     * ASCII Code : '8'
+     */
+    public static final byte ASCII_8 = (byte) 0x38;
+    /**
+     * ASCII Code : '9'
+     */
+    public static final byte ASCII_9 = (byte) 0x39;
+    /**
+     * ASCII Code : ':'
+     */
+    public static final byte ASCII_COLON = (byte) 0x3a;
+    /**
+     * ASCII Code : ';'
+     */
+    public static final byte ASCII_SEMI_COLON = (byte) 0x3b;
+    /**
+     * ASCII Code : '<'
+     */
+    public static final byte ASCII_LESS_THAN = (byte) 0x3c;
+    /**
+     * ASCII Code : '='
+     */
+    public static final byte ASCII_EQUAL = (byte) 0x3d;
+    /**
+     * ASCII Code : '>'
+     */
+    public static final byte ASCII_GREATER_THAN = (byte) 0x3e;
+    /**
+     * ASCII Code : '?'
+     */
+    public static final byte ASCII_QUESTION = (byte) 0x3f;
+    /**
+     * ASCII Code : '@'
+     */
+    public static final byte ASCII_AT = (byte) 0x40;
+    /**
+     * ASCII Code : 'A'
+     */
+    public static final byte ASCII_A = (byte) 0x41;
+    /**
+     * ASCII Code : 'B'
+     */
+    public static final byte ASCII_B = (byte) 0x42;
+    /**
+     * ASCII Code : 'C'
+     */
+    public static final byte ASCII_C = (byte) 0x43;
+    /**
+     * ASCII Code : 'D'
+     */
+    public static final byte ASCII_D = (byte) 0x44;
+    /**
+     * ASCII Code : 'E'
+     */
+    public static final byte ASCII_E = (byte) 0x45;
+    /**
+     * ASCII Code : 'F'
+     */
+    public static final byte ASCII_F = (byte) 0x46;
+    /**
+     * ASCII Code : 'G'
+     */
+    public static final byte ASCII_G = (byte) 0x47;
+    /**
+     * ASCII Code : 'H'
+     */
+    public static final byte ASCII_H = (byte) 0x48;
+    /**
+     * ASCII Code : 'I'
+     */
+    public static final byte ASCII_I = (byte) 0x49;
+    /**
+     * ASCII Code : 'J'
+     */
+    public static final byte ASCII_J = (byte) 0x4a;
+    /**
+     * ASCII Code : 'K'
+     */
+    public static final byte ASCII_K = (byte) 0x4b;
+    /**
+     * ASCII Code : 'L'
+     */
+    public static final byte ASCII_L = (byte) 0x4c;
+    /**
+     * ASCII Code : 'M'
+     */
+    public static final byte ASCII_M = (byte) 0x4d;
+    /**
+     * ASCII Code : 'N'
+     */
+    public static final byte ASCII_N = (byte) 0x4e;
+    /**
+     * ASCII Code : 'O'
+     */
+    public static final byte ASCII_O = (byte) 0x4f;
+    /**
+     * ASCII Code : 'P'
+     */
+    public static final byte ASCII_P = (byte) 0x50;
+    /**
+     * ASCII Code : 'Q'
+     */
+    public static final byte ASCII_Q = (byte) 0x51;
+    /**
+     * ASCII Code : 'R'
+     */
+    public static final byte ASCII_R = (byte) 0x52;
+    /**
+     * ASCII Code : 'S'
+     */
+    public static final byte ASCII_S = (byte) 0x53;
+    /**
+     * ASCII Code : 'T'
+     */
+    public static final byte ASCII_T = (byte) 0x54;
+    /**
+     * ASCII Code : 'U'
+     */
+    public static final byte ASCII_U = (byte) 0x55;
+    /**
+     * ASCII Code : 'V'
+     */
+    public static final byte ASCII_V = (byte) 0x56;
+    /**
+     * ASCII Code : 'W'
+     */
+    public static final byte ASCII_W = (byte) 0x57;
+    /**
+     * ASCII Code : 'X'
+     */
+    public static final byte ASCII_X = (byte) 0x58;
+    /**
+     * ASCII Code : 'Y'
+     */
+    public static final byte ASCII_Y = (byte) 0x59;
+    /**
+     * ASCII Code : 'Z'
+     */
+    public static final byte ASCII_Z = (byte) 0x5a;
+    /**
+     * ASCII Code : '['
+     */
+    public static final byte ASCII_L_BRACE = (byte) 0x5b;
+    /**
+     * ASCII Code : '\'
+     */
+    public static final byte ASCII_BACK_SLASH = (byte) 0x5c;
+    /**
+     * ASCII Code : ']'
+     */
+    public static final byte ASCII_R_BRACE = (byte) 0x5d;
+    /**
+     * ASCII Code : '^'
+     */
+    public static final byte ASCII_SQRT = (byte) 0x5e;
+    /**
+     * ASCII Code : '_'
+     */
+    public static final byte ASCII_UNDER_SCORE = (byte) 0x5f;
+    /**
+     * ASCII Code : '`'
+     */
+    public static final byte ASCII_BACKQUOTE = (byte) 0x60;
+    /**
+     * ASCII Code : 'a'
+     */
+    public static final byte ASCII_a = (byte) 0x61;
+    /**
+     * ASCII Code : 'b'
+     */
+    public static final byte ASCII_b = (byte) 0x62;
+    /**
+     * ASCII Code : 'c'
+     */
+    public static final byte ASCII_c = (byte) 0x63;
+    /**
+     * ASCII Code : 'd'
+     */
+    public static final byte ASCII_d = (byte) 0x64;
+    /**
+     * ASCII Code : 'e'
+     */
+    public static final byte ASCII_e = (byte) 0x65;
+    /**
+     * ASCII Code : 'f'
+     */
+    public static final byte ASCII_f = (byte) 0x66;
+    /**
+     * ASCII Code : 'g'
+     */
+    public static final byte ASCII_g = (byte) 0x67;
+    /**
+     * ASCII Code : 'h'
+     */
+    public static final byte ASCII_h = (byte) 0x68;
+    /**
+     * ASCII Code : 'i'
+     */
+    public static final byte ASCII_i = (byte) 0x69;
+    /**
+     * ASCII Code : 'j'
+     */
+    public static final byte ASCII_j = (byte) 0x6a;
+    /**
+     * ASCII Code : 'k'
+     */
+    public static final byte ASCII_k = (byte) 0x6b;
+    /**
+     * ASCII Code : 'l'
+     */
+    public static final byte ASCII_l = (byte) 0x6c;
+    /**
+     * ASCII Code : 'm'
+     */
+    public static final byte ASCII_m = (byte) 0x6d;
+    /**
+     * ASCII Code : 'n'
+     */
+    public static final byte ASCII_n = (byte) 0x6e;
+    /**
+     * ASCII Code : 'o'
+     */
+    public static final byte ASCII_o = (byte) 0x6f;
+    /**
+     * ASCII Code : 'p'
+     */
+    public static final byte ASCII_p = (byte) 0x70;
+    /**
+     * ASCII Code : 'q'
+     */
+    public static final byte ASCII_q = (byte) 0x71;
+    /**
+     * ASCII Code : 'r'
+     */
+    public static final byte ASCII_r = (byte) 0x72;
+    /**
+     * ASCII Code : 's'
+     */
+    public static final byte ASCII_s = (byte) 0x73;
+    /**
+     * ASCII Code : 't'
+     */
+    public static final byte ASCII_t = (byte) 0x74;
+    /**
+     * ASCII Code : 'u'
+     */
+    public static final byte ASCII_u = (byte) 0x75;
+    /**
+     * ASCII Code : 'v'
+     */
+    public static final byte ASCII_v = (byte) 0x76;
+    /**
+     * ASCII Code : 'w'
+     */
+    public static final byte ASCII_w = (byte) 0x77;
+    /**
+     * ASCII Code : 'x'
+     */
+    public static final byte ASCII_x = (byte) 0x78;
+    /**
+     * ASCII Code : 'y'
+     */
+    public static final byte ASCII_y = (byte) 0x79;
+    /**
+     * ASCII Code : 'z'
+     */
+    public static final byte ASCII_z = (byte) 0x7a;
+    /**
+     * ASCII Code : '{'
+     */
+    public static final byte ASCII_L_CURLY_BRACE = (byte) 0x7b;
+    /**
+     * ASCII Code : '|'
+     */
+    public static final byte ASCII_PIPE = (byte) 0x7c;
+    /**
+     * ASCII Code : '}'
+     */
+    public static final byte ASCII_R_CURLY_BRACE = (byte) 0x7d;
+    /**
+     * ASCII Code : '~'
+     */
+    public static final byte ASCII_TILT = (byte) 0x7e;
+    /**
+     * ASCII Code : DEL
+     */
+    public static final byte ASCII_DEL = (byte) 0x7f;
+}
