7189bb382c34d9062ff171214b68409799a25149
Sangmin Lee <lempel@gmail.com>
2015-12-21 16:30:19 +0900

    License agreement added

--- blueprint-sdk/src/blueprint/sdk/util/AsciiTable.java
+++ blueprint-sdk/src/blueprint/sdk/util/AsciiTable.java
@@ -1,3 +1,16 @@
+/*
+ License:
+
+ blueprint-sdk is licensed under the terms of Eclipse Public License(EPL) v1.0
+ (http://www.eclipse.org/legal/epl-v10.html)
+
+
+ Distribution:
+
+ Repository - https://github.com/lempel/blueprint-sdk.git
+ Blog - http://lempel.egloos.com
+ */
+
 package blueprint.sdk.util;
 
 /**
